87451af79ea210c9a1a57ba8d0b0894c6441cc7f
LanthPate <acnewobj@gmail.com>
2016-07-19 19:12:12 -0700

    initial dev file
    initial edit #1

---
+++ easy.dev.1.0.js
@@ -0,0 +1,10 @@
+!(function(frame,page,body){
+  var isReady = false,
+      frameWidth = frame.innerWidth,
+      frameHeight = frame.innerHeight,
+      _easyLoc = location,
+      _easyLocHref = location.href,
+      _easyHttpProtocol = location.protocol,
+      local_easy;
+  window.easy = local_easy || {};
+})(window,document,document.body)
